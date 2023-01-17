ff9d6e8e215e098f40ada00debe30ef6fcc5785f
Mikhail <goitw@outlook.com>
2023-01-17 13:35:49 +0100

    t1-4 done

---
+++ t2/t2.cs
@@ -0,0 +1,17 @@
+﻿int input_int(string msg)
+{
+    Console.Write(msg);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int a = input_int ("Put three-digit number(xxx): ");
+if (a > 99 && a < 1000)
+{
+    int f = a % 100;
+    int res = f / 10;
+    Console.WriteLine($"The second number of {a} is {res}");
+}
+else
+{
+    Console.WriteLine($"The number isn't three-digit.");
+}
